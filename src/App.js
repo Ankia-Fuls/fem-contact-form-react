d89b9ba87db1c0850059afe0554c495ac7b3a3b2
Ankia-Fuls <ankiafuls01@gmail.com>
2025-08-27 14:51:47 +0200

    Changed Attribution

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
 
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-        {/* Coded by <a href="#">Your Name Here</a>. */}
+        Coded by <a href="https://github.com/Ankia-Fuls">Ankia Fuls</a>.
       </div>
     </main>
   );

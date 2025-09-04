768a8a39181e37ebe4324ea60c06689e5a0ac0ea
Ankia-Fuls <ankiafuls01@gmail.com>
2025-09-04 14:35:43 +0200

    Button and required symbol

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,26 +5,26 @@ function Main() {
                 <h1 className="form__heading">Contact Us</h1>
                 <fieldset className="form__name">
                     <div className="form__name--first">
-                        <label for="first-name">First Name</label>
+                        <label for="first-name">First Name <span className="form__required" aria-label="Required input.">*</span></label>
                         <input type="text" id="first-name" name="first-name"></input>
                         <p className="form__error">This field is required</p>
                     </div>
                     <div className="form__name--last">
-                        <label for="last-name">Last Name</label>
+                        <label for="last-name">Last Name <span className="form__required" aria-label="Required input.">*</span></label>
                         <input type="text" id="last-name" name="last-name"></input>
                         <p className="form__error">This field is required</p>
                     </div>
                 </fieldset>
 
                 <fieldset className="form__email">
-                    <label for="email">Email Address</label>
+                    <label for="email">Email Address <span className="form__required" aria-label="Required input.">*</span></label>
                     <input type="email" id="email" name="email"></input>
                     <p className="form__error form__error--valid">Please enter a valid email address</p>
                     <p className="form__error">This field is required</p>
                 </fieldset>
 
                 <fieldset className="form__type">
-                    <legend>Query Type</legend>
+                    <legend>Query Type <span className="form__required" aria-label="Required input.">*</span></legend>
                     <div className="radio-buttons">
                         <div className="form__container">
                             <input type="radio" id="general" name="type" value="general" />
@@ -39,14 +39,14 @@ function Main() {
                 </fieldset>
 
                 <fieldset className="form__message">
-                    <legend>Message</legend>
+                    <legend>Message <span className="form__required" aria-label="Required input.">*</span></legend>
                     <textarea name="message"></textarea>
                     <p className="form__error">This field is required</p>
                 </fieldset>
 
                 <fieldset className="form__consent">
                     <input type="checkbox" id="consent" name="consent"></input>
-                    <label for="consent">I consent to being contacted by the team</label>
+                    <label for="consent">I consent to being contacted by the team <span className="form__required" aria-label="Required input.">*</span></label>
                     <p className="form__error">To submit this form, please consent to being contacted</p>
                 </fieldset>
 
